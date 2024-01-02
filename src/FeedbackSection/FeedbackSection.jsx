2e0f6dc380f6786c78457a918c4a7247d2a4e25b
Maksym Pysmennyi <maksimviktorovichua@gmail.com>
2024-01-02 10:01:25 +0100

    state_obgect

--- src/FeedbackSection/FeedbackSection.jsx
+++ src/FeedbackSection/FeedbackSection.jsx
@@ -6,13 +6,29 @@ import Button from '../Button/Button'
 import './FeedbackSection.css'
 
 const FeedbackSection = () => {
-  const [name, setName] = useState('')
-  const [hasError, setHesError] = useState(false)
-  const [reason, setReason] = useState('Server')
+  const [form, setForm] = useState(
+    {
+      name: '',
+      hasError: false,
+      reason: 'Server',
+    }
+  )
+
+  // const [name, setName] = useState('')
+  // const [hasError, setHesError] = useState(false)
+  // const [reason, setReason] = useState('Server')
 
   function handleNameChange(event) {
-    setName(event.target.value)
-    setHesError(event.target.value.trim().length === 0)
+    // setForm({
+    //   name: event.target.value,
+    //   hasError: event.target.value.trim().length === 0,
+    //   reason: form.reason,
+    // })
+    setForm((prev)=>({
+      ...prev,
+      name: event.target.value,
+      hasError: event.target.value.trim().length === 0,
+    }))
   }
 
   return (
@@ -26,28 +42,28 @@ const FeedbackSection = () => {
               type="text" 
               id='name' 
               className='control' 
-              value={name} 
+              value={form.name} 
               onChange={handleNameChange}
               style={{
-                border: hasError ? '2px solid red' : null,
+                border: form.hasError ? '2px solid red' : null,
               }}
               />
 
             <label htmlFor="reason">The reason for petition</label>
-            <select id="reason" className='control' value={reason} onChange = {(event)=> setReason(event.target.value)}>
+            <select id="reason" className='control' value={form.reason} onChange = {(event)=> setForm(prev=>({...prev, reason: event.target.value}))}>
               <option value="Informational">Informational responses (100 – 199)</option>
               <option value="Successful">Successful responses (200 – 299)</option>
               <option value="Redirection">Redirection messages (300 – 399)</option>
               <option value="Client">Client error responses (400 – 499)</option>
               <option value="Server">Server error responses (500 – 599)</option>
             </select>
-            <Button disabled = {hasError }>
+            <Button disabled = {form.hasError }>
               Send
             </Button>
         </form>
         <pre>
-          Name:{name}
-          Reason:{reason}
+          Name:{form.name}
+          Reason:{form.reason}
         </pre>
     </section>
   )
